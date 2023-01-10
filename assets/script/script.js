279c89df9d7b65124987df1425e755d18216a410
Jacob Jeffries <jeffrij85@gmail.com>
2023-01-09 19:43:07 -0500

    started playing with mapbox

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,12 @@
-let origin = []
-let destination = []
+let origin = ['marquette', 'MI']
+let destination = ['jackson','MI']
+
+let mapboxgl.accessToken = 'pk.eyJ1IjoiamFjb2ItamVmZnJpZXMiLCJhIjoiY2xjcDJzeTJtMWh3YzNwcjBscWJ2amg5OCJ9.FCsyRgLMa5gW0lyMlWsClw';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [-87, 46], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+  });
